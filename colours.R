9ca1df6e412be229cc406d41a9c4449988d0a2f6
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-12-30 19:14:56 -0500

    Initial commit

---
+++ colours.R
@@ -0,0 +1,30 @@
+genuary_colours <- function() {
+  c(
+    red = "#F14A14",
+    orange = "#FF9316",
+    yellow = "#FBC21B",
+    darkpink = "#F65A82",
+    lightpink = "#FD96B8",
+    darkblue = "#4A3DFB",
+    lightblue = "#8DD8F8",
+    purple = "#B38AD9",
+    darkgreen = "#459C00",
+    lightgreen = "#C1F213",
+    dark = "#313131",
+    light = "#FFF5F6"
+  )
+}
+
+red <- "#F14A14"
+orange <- "#FF9316"
+yellow <- "#FBC21B"
+darkpink <- "#F65A82"
+lightpink <- "#FD96B8"
+darkblue <- "#4A3DFB"
+lightblue <- "#8DD8F8"
+purple <- "#B38AD9"
+darkgreen <- "#459C00"
+lightgreen <- "#C1F213"
+dark <- "#313131"
+light <- "#FFF5F6"
+
